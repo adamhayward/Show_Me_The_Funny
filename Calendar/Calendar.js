55339561ea0ff4bf8e91cf2bbaf861188e88008e
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2020-11-13 11:45:08 -0600

    commit to updated-calendar

--- Calendar/Calendar.js
+++ Calendar/Calendar.js
@@ -1,83 +1,82 @@
-function settingDate(date,day) {
-date=new Date(date);
-date.setDate(day);
-date.setHours(23);
-return date; 
-}
-function getDatesBetween(date1, date2) {
-    let range1 = new Date(date1);
-    let range2 = new Date(date2);
-    date1 = settingDate(date1, 31);
-    date2 = settingDate(date2, 31);
-    let temp;
-    let dates = [];
-    while (date1 <= date2) {
-        if (date1.getDate() != 31) {
-            temp = settingDate (date1, 0);
-            if (temp >= range1 && temp <= range2) dates.push(temp);
-            date1 = settingDate(date1, 31);
-        } else {
-            temp= new Date(date1);
-            if(temp >= range1 && temp <= range2) dates.push(temp);
-            date1.setMonth(date1.getMonth() + 1);
-        }
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
     }
-    console.log(dates);
-    let content = "";
-    let weekDays = [
-        { shortDay: "Mon", fullDay: "Monday"},
-        { shortDay: "Tue", fullDay: "Tuesday"},
-        { shortDay: "Wed", fullDay: "Wednesday"},
-        { shortDay: "Thu", fullDay: "Thursday"},
-        { shortDay: "Fri", fullDay: "Friday"},
-        { shortDay: "Sat", fullDay: "Saturday"},
-        { shortDay: "Sun", fullDay: "Sunday"},
-    ];
-    let LastDate, firstDate; 
-    for (let i=0; i < dates.length; i++) {
-        LastDate = dates[i];
-        firstDate = new Date(dates[i].getFullYear(), dates[1].getMonth(), 1);
-        content += "<div id= 'calendarTable" + (i + 1) + "'>";
-        content += 
-        "<h2>" + 
-        firstDate.toString().split(" ")[0] + 
-        "-" + 
-        firstDate.getFullYear() + 
-        "</h2>";
-        content += "<table class='calendarTable'>";
-        content += "<thead >"; 
-        weekDays.map(item=>{
-            content+="<th>"+item.fullDay+"</th>";
-        });
-        content += "</thead>";
-        content += "<tbody >";
-        let j=1;
-        let displayNum, idMonth; 
-        while (j <= LastDate.getDate()) {
-            content += "<tr>";
-            for (let k = 0; k < 7; k++) {
-                displayNum = j < 10 ? "0" + j : j; 
-                if(j == 1) { 
-                    if(firstDate.toString().split(" ")[0] == weekDays[k].shortDay) {
-                        content += "<td>" + displayNum + "</td>";
-                        j++;    
-                    } else {
-                        content += "<td></td>";
-                    }
-                }else if(j>LastDate.getDate()){
-                    content += "<td></td>";
-                } else {
-                    content += "<td>" + displayNum + "</td>"; 
-                    j++
-                }
-            }
-            content += "</tr>";
-        }
-        content += "</tbody >";
-        content += "</table>";
-        content += "</div>";
-    }
-    return content;
-}
-let content = getDatesBetween("2020/01/01", "2021/01/01");
-document.getElementById("calendar").innerHTML = content;
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
